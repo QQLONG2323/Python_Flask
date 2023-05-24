7e1839d3217c967b7ba499cdd215a046faa9fdd6
余友中 <qqlong2323@gmail.com>
2023-05-24 11:50:00 +0800

    modify

--- 2023_05_11/app.py
+++ 2023_05_11/app.py
@@ -3,23 +3,30 @@ import datasource
 
 
 app = Flask(__name__)
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def index():
-    stock_data = datasource.get_stock_data(stockid=2330)
-    return render_template("index.jinja.html", data=stock_data)
+    rows = datasource.get_stockid()
+    if request.method == 'POST':
+        stock_name = request.form['stock_name']  #1203-味王
+        stock_id = stock_name[:4]        
+        year =  request.form['year']
+        stock_dataFrame = datasource.get_stock_data(stockid=stock_id,year=year)  
+        stock_data = stock_dataFrame.to_numpy().tolist()
+        return render_template("form.jinja.html",rows=rows,stock_name=stock_name,year=year,data=stock_data)  
+    return render_template("form.jinja.html",rows=rows)
 
 
-@app.route("/features/")
+@app.route("/features")
 def features():
 
     return render_template("features.jinja.html")
 
-@app.route("/pricing/")
+@app.route("/pricing")
 def pricing():
     
     return render_template("pricing.jinja.html")
 
-@app.route("/about/")
+@app.route("/about")
 def about():
     return render_template("about.jinja.html")
 
